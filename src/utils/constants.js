788677bd4cbef30ea7f0033cf9be1071fa2c174c
amarjeetk123 <ak123del@gmail.com>
2022-11-11 16:26:43 +0530

    compliting video details section

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -30,7 +30,7 @@ export const categories = [
   { name: 'Fashion', icon: <CheckroomIcon />, },
   { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
   { name: 'Comedy', icon: <TheaterComedyIcon />, },
-  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  // { name: 'Gym', icon: <FitnessCenterIcon />, },
   { name: 'Crypto', icon: <DeveloperModeIcon />, },
 ];
 
